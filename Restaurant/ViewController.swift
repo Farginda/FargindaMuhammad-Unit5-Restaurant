6bdba75964ff6f8a84ba74aed319b404dfffae63
Farginda <Farginda@luuks-macbook-pro.local>
2018-12-04 19:13:57 +0100

    Initial Commit

---
+++ Restaurant/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Restaurant
+//
+//  Created by Farginda on 12/4/18.
+//  Copyright © 2018 Farginda. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
